187513d22693b16594a5758cffea0dcbb5cb3ed5
Dave Flores Malhin <suppordev199x@gmail.com>
2023-04-27 17:16:04 +0800

    Update for deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
     <footer>
       <p>
         Contact me at&nbsp;
-        <a style={{ fontWeight: "bold", color: "black" }}>
+        <b style={{ fontWeight: "bold", color: "black" }}>
           malhindave@gmail.com
-        </a>
+        </b>
       </p>
       <p>123 Main St, City, State 12345</p>
       <p>555-555-5555</p>
